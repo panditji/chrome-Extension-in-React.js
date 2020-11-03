8ffccf9aa8ae7b161d6bb02c79d44d9002498e59
panditji <ankush21191@gmail.com>
2020-11-03 23:27:14 +0530

    Add files via upload

---
+++ build/precache-manifest.47bcba443430ad377da332afe772b613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19522f16b8baf148dcd38d1863bfa4a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c160cdfdfb4ba88792",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7fb2a34ae16f9456fabb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8c160cdfdfb4ba88792",
+    "url": "/static/js/2.dbede8c2.chunk.js"
+  },
+  {
+    "revision": "b818062cb6a02d6d46e1d6036acdfc48",
+    "url": "/static/js/2.dbede8c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb2a34ae16f9456fabb",
+    "url": "/static/js/main.f49d5e20.chunk.js"
+  },
+  {
+    "revision": "64c1e5fce6f564a0eb92",
+    "url": "/static/js/runtime-main.114859f1.js"
+  }
+]);
